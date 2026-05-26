083dfcebdae8c57b1d546592dd34cda0382f660d
Yafet <yafetman44@gmail.com>
2026-05-26 10:02:56 -0500

    updated the memory layer

--- Chat_buffer/memory/__init__.py
+++ Chat_buffer/memory/__init__.py
@@ -1,3 +1,2 @@
 from memory.Chatbuffer import ChatBuffer
-
 chat_buffer=ChatBuffer(max_messages=10)
---
+++ Memory/Rulelayer/rulelayer.py
@@ -0,0 +1,24 @@
+class Rulelayer:
+    def __init__(self):
+        self.rules=[
+            "i  studied",
+            "i slept",
+            "i worked",
+            "today i",
+            "yesterday i"
+        ]
+        self.profile=[
+            "i prefer",
+            "i usually",
+            "i always",
+            "i struggle with",
+            "i am good at"
+
+        ]
+        self.ignore=[
+            "ok",
+            "lol",
+            "haha",
+            "nice",
+            "cool"
+ ]
